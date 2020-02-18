a7fd50f3ddf8d4f8a326d760e04451eb58c84b71
Nikhil Lal Kolangara <nikvikolangara@gmail.com>
2020-02-18 01:42:56 -0500

    Implemented Sorting of Elements and tested first prototype

--- 8 Puzzle Problem/8 Puzzle Problem.py
+++ 8 Puzzle Problem/8 Puzzle Problem.py
@@ -11,9 +11,21 @@ import pandas as pd
 print("Enter the initial state of the Puzzle:")
 
 initialState = []
-goalState = []
+goalState = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 for i in range(0,9):
     print("Enter the value:")
     value = int(input())
     initialState.append(value)
+    
+print(initialState)
+
+for i in range(0,len(initialState)):
+    for j in range(0, len(initialState) - i - 1):    
+        if initialState[j] > initialState[j+1]:
+            temp = initialState[j]
+            initialState[j] = initialState[j+1]
+            initialState[j+1] = temp
+        
+print(initialState)
+        
