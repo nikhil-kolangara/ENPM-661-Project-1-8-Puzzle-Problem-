29279e35fc688f350bfc30c5afea2622c616f801
Nikhil Lal Kolangara <nikvikolangara@gmail.com>
2020-02-17 23:51:08 -0500

    Added List as Data Structures to store the initial state and included code to take input from User

--- 8 Puzzle Problem/8 Puzzle Problem.py
+++ 8 Puzzle Problem/8 Puzzle Problem.py
@@ -7,3 +7,13 @@ Created on Mon Feb 17 22:49:20 2020
 
 import numpy as np
 import pandas as pd
+
+print("Enter the initial state of the puzzle:")
+
+InitialState = []
+GoalState = []
+
+for i in range(0,8):
+    print("Enter the value:")
+    InitialState[i] = input()
+    
