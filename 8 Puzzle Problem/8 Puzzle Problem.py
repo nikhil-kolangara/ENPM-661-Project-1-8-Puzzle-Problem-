44a77ad38bfce985177dc06a96a86563046edd60
Nikhil Lal Kolangara <nikvikolangara@gmail.com>
2020-02-22 00:51:15 -0500

    Implemented solvable and input check functions

--- 8 Puzzle Problem/8 Puzzle Problem.py
+++ 8 Puzzle Problem/8 Puzzle Problem.py
@@ -7,20 +7,83 @@ Created on Mon Feb 17 22:49:20 2020
 
 import numpy as np
 
-print("Enter the initial state of the Puzzle:")
+# structure to store parameters
 
-initialState = []
-a = []
-goalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+class Node:
+    def __init__(self, node_no, data, parent, act, cost):
+        self.data = data
+        self.parent = parent
+        self.act = act
+        self.node_no = node_no
+        self.cost = cost
 
-for i in range(3):
-    for j in range(3):
-        print("Enter the value:")
-        a.append(int(input()))
-    initialState.append(a)
+def initialNode():
+    print("Enter the initial state of the Puzzle:")
+    initialState = []
+    userInput = []
+    goalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
+    for i in range(3):
+        for j in range(3):
+            print("Enter the value:")
+            value = int(input())
+            if (value > 8) or (value < 0):
+                print("Please enter values between 0 to 8")
+            else: 
+                userInput.append(int(input()))
+                initialState.append(userInput)
         
-initialState = np.transpose(initialState)
-print(initialState)
+    initialState = np.transpose(initialState)
+    print(initialState)
+    
+def isSolvable(initialState):
+    solvable = initialState
+    count = 0
+    for i in range(9):
+        if solvable[i] != 0:
+            value = solvable[i]
+            for j in range(i + 1, 9):
+                if value < solvable[j] or solvable[j] == 0:
+                    continue
+                else:
+                    count = count + 1
+    if (count % 2) == 0:
+        print("The Puzzle is solvable.")
+    else:
+        print("The puzzle is insolvable.")
+        
+def isCorrect(initialState):
+    correct = initialState
+    for i in range(9):
+        count = 0
+        value = array[i]
+        for j in range(9):
+            if value == array[j]:
+                count = count + 1
+        if count >= 2:
+            print("Number inputted Twice, Cannot Proceed!")
+            exit(0)
+
+    
+
+
+
+def actionMoveLeft(initialState):
+    if((initialState[i] != 0) && initialState[j] != 0,1,2):
+        
+    
+def actionMoveRight(initialState):
+    if((initialState[i] != 0,1,2) && initialState[j] != 2):
+        
+    
+def actionMoveUp(initialState):
+    if((initialState[i] != 0) && (initialState[j] != 0)):
+        initialState[i-1][j] = initialState[i][j]
+    
+def actionMoveDown(initialState[i]):
+    if((initialState[i] != 2) && (initialState[j] != 2):
+        initialState[i+1][j] = initialState[i][j]
+        
 
 def blankTileLocation(initialState):
     for i in range (3):
@@ -32,17 +95,6 @@ def blankTileLocation(initialState):
 [i,j] = blankTileLocation()
 print(i,j)
     
-#print(initialState)
-"""
-for i in range(0,len(initialState)):
-    for j in range(0, len(initialState) - i - 1):    
-        if initialState[j] > initialState[j+1]:
-            temp = initialState[j]
-            initialState[j] = initialState[j+1]
-            initialState[j+1] = temp
-        
-print(initialState)
-"""
 
 
         
