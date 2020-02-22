58f3a2d4d8c2243594c637f962c16d37a96987cf
Nikhil Lal Kolangara <nikvikolangara@gmail.com>
2020-02-21 19:47:56 -0500

    Implemented Action Functions

--- 8 Puzzle Problem/8 Puzzle Problem.py
+++ 8 Puzzle Problem/8 Puzzle Problem.py
@@ -6,20 +6,34 @@ Created on Mon Feb 17 22:49:20 2020
 """
 
 import numpy as np
-import pandas as pd
 
 print("Enter the initial state of the Puzzle:")
 
 initialState = []
-goalState = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+a = []
+goalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
-for i in range(0,9):
-    print("Enter the value:")
-    value = int(input())
-    initialState.append(value)
-    
+for i in range(3):
+    for j in range(3):
+        print("Enter the value:")
+        a.append(int(input()))
+    initialState.append(a)
+        
+initialState = np.transpose(initialState)
 print(initialState)
 
+def blankTileLocation(initialState):
+    for i in range (3):
+        for j in range(3):
+            if initialState[i][j] == 0:
+                break
+    return(i,j)
+            
+[i,j] = blankTileLocation()
+print(i,j)
+    
+#print(initialState)
+"""
 for i in range(0,len(initialState)):
     for j in range(0, len(initialState) - i - 1):    
         if initialState[j] > initialState[j+1]:
@@ -28,4 +42,7 @@ for i in range(0,len(initialState)):
             initialState[j+1] = temp
         
 print(initialState)
+"""
+
+
         
